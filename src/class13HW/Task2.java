8367a51d4307ac24a40e65802cda75c30fe5642c
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-01-25 18:11:25 -0500

    Second commit Class 13 HW

--- src/class13HW/Task2.java
+++ src/class13HW/Task2.java
@@ -4,9 +4,10 @@ public class Task2 {
     public static void main(String[] args) {
         String str="a12d$%d$%gzbL&%^G";
 
-       int alphaNumeric=str.replaceAll("[^a-zA-Z1-9]","").length();
+       String alphaNumeric=str.replaceAll("[^a-zA-Z1-9]","");
+      int alphaNumericLength=alphaNumeric.length();
 
-        System.out.println("The total quantity of alphanumeric characters in the string is "+ alphaNumeric);
+        System.out.println("The total quantity of alphanumeric characters in the string is "+ alphaNumericLength);
 
 
 
--- src/class13HW/Task4.java
+++ src/class13HW/Task4.java
@@ -2,9 +2,15 @@ package class13HW;
 
 public class Task4 {
     public static void main(String[] args) {
-StringBuilder str= new StringBuilder("This is the sentence i want to reverse");
-        System.out.println(str);
-        System.out.println(str.reverse());
+String str="This is the sentence i want to reverse";
+       String[] strSplit=str.split(" ");
+       String reverseWord="";
+       for (String x: strSplit){
+           StringBuilder sb= new StringBuilder(x);
+           sb.reverse();
+           reverseWord+= sb.toString()+" ";
+       }
+        System.out.println(reverseWord);
 
     }
 }
