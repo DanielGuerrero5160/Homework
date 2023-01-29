b5ce21b4bcdc78ec6aa1705eb3cbe8a94a5131f9
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-01-29 15:09:56 -0500

    Class 16 HW

---
+++ src/Class16HW/HWTask1.java
@@ -0,0 +1,17 @@
+package Class16HW;
+
+public class HWTask1 {
+    int arrSum(int [] arr) {
+        int sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        HWTask1 task1=new HWTask1();
+        int[] arr={1,2,3,4,5};
+        System.out.println(task1.arrSum(arr));
+    }
+}
---
+++ src/Class16HW/HWTask2.java
@@ -0,0 +1,15 @@
+package Class16HW;
+
+public class HWTask2 {
+    public String revStr(String str){
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        HWTask2 task2=new HWTask2();
+        System.out.println(task2.revStr("Hello"));
+
+    }
+}
